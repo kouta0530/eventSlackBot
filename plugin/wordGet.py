430d619a0a51c7f1618a759aab0803644d99d958
kouta0530 <16jk047@ymklab.net>
2020-08-15 14:33:14 +0900

    mecabプログラムを用意

--- plugin/wordGet.py
+++ plugin/wordGet.py
@@ -2,8 +2,7 @@
 import MeCab
 
 def mecab():
-    mecab = MeCab.Tagger("")
-    text = """今日の天気は晴れです．"""
-    #nouns = [line.split()[0] for line in mecab.parse(text).splitlines()
-    #            if "名詞" in line.split()[-1]]
+    mecab = MeCab.Tagger()
+    text = "今日の天気は晴れです．"
+    nouns = [line.split()[0] for line in mecab.parse(text).splitlines()if "名詞" in line.split()[-1]]
     return text
