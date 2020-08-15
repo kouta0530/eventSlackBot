e28450bbb6ecb40b5f3fa4ad89300588d7e48c20
kouta0530 <16jk047@ymklab.net>
2020-08-15 15:32:53 +0900

    test

--- plugin/wordGet.py
+++ plugin/wordGet.py
@@ -1,14 +1,7 @@
 # -*- coding: utf-8 -*-
+from janome.tokenizer import Tokenizer
 
 def mecab():
-    """
-    mecab = MeCab.Tagger()
-    text = 今日の天気は晴れです．
-    nouns = [line.split()[0] for line in mecab.parse(text).splitlines()if "名詞" in line.split()[-1]]
-    return text
-    """
-
-    from janome.tokenizer import Tokenizer
     t = Tokenizer()
     malist = t.tokenize("今日の天気は晴れです")
     
