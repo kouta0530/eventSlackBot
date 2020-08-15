69dedb80e5bf61c59bb8804aefdb2f59ea07d3d4
kouta0530 <16jk047@ymklab.net>
2020-08-15 14:54:42 +0900

    janomeに変更

--- plugin/wordGet.py
+++ plugin/wordGet.py
@@ -1,8 +1,17 @@
 # -*- coding: utf-8 -*-
-import MeCab
 
 def mecab():
+    """
     mecab = MeCab.Tagger()
-    text = "今日の天気は晴れです．"
+    text = 今日の天気は晴れです．
     nouns = [line.split()[0] for line in mecab.parse(text).splitlines()if "名詞" in line.split()[-1]]
     return text
+    """
+
+    from janome.tokenizer import Tokenizer
+    t = Tokenizer()
+    malist = t.tokenize("今日の天気は晴れです")
+    
+    word = [n.surface for n in malist]
+
+    return word
