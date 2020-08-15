3d58330efc383c87feaa262d351e16b2e5e92cff
kouta0530 <16jk047@ymklab.net>
2020-08-15 21:22:06 +0900

    test

--- plugin/wordGet.py
+++ plugin/wordGet.py
@@ -1,6 +1,7 @@
-# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*
+from janome.tokenizer import Tokenizer
+
 def mecab():
-    from janome.tokenizer import Tokenizer
     t = Tokenizer(wakati = True)
     malist = t.tokenize("今日の天気は晴れです",wakati= True,stream = True)
     malist = ",".join(malist)
