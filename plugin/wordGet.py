d0fff1a2766c9c59b532d816d1dc419aee7f2f6c
kouta0530 <16jk047@ymklab.net>
2020-08-15 19:00:36 +0900

    streamの追加

--- plugin/wordGet.py
+++ plugin/wordGet.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 def mecab():
     from janome.tokenizer import Tokenizer
-    t = Tokenizer()
-    malist = t.tokenize("今日の天気は晴れです",wakati= True)
+    t = Tokenizer(wakati = True)
+    malist = t.tokenize("今日の天気は晴れです",wakati= True,stream = True)
+    malist = ",".join(malist)
     
     return malist
