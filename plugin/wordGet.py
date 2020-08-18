acd461792d942643ff94ee231d19f3c7faa56ffa
kouta0530 <16jk047@ymklab.net>
2020-08-18 20:51:59 +0900

    分かち書きの品詞を

--- plugin/wordGet.py
+++ plugin/wordGet.py
@@ -3,7 +3,8 @@ from janome.tokenizer import Tokenizer
 
 def mecab(text):
     t = Tokenizer()
-    malist = t.tokenize(text,wakati= True)
-    #malist = ",".join(malist)
+    malist = t.tokenize(text)
+
+    words =  [i.surface for i in malist if i.part_of_speech.split(",")[0] == "名詞"]
     
-    return malist
+    return words
